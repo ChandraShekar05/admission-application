2fb5d6df22b05fd42a1e9cc14da279b94ac4bf0c
Sidharth Punna <sidharth.punna304@gmail.com>
2024-09-25 22:34:02 +0530

    duplicate fix

--- client/src/pages/coursesPage.jsx
+++ client/src/pages/coursesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import CourseTable from '../components/admin/CourseTable';
 import { getAllCourses } from '../services/coursesApi';
 
